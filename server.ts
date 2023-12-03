75c4b301309e1d10d75b2a91d7125315e96c4fc7
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-12-03 11:02:21 +0800

    feat: finished upload and download flow

--- server.ts
+++ server.ts
@@ -7,28 +7,45 @@ let app = express()
 let server = http.createServer(app)
 let io = new socketIO.Server(server)
 
-// slug -> name -> size
-let rooms: Record<string, Record<string, number>> = {}
+type FileMeta = {
+  size: number
+  type: string
+  lastModified: number
+}
+// slug -> name -> FileMeta
+let rooms: Record<string, Record<string, FileMeta>> = {}
 
 io.on('connection', socket => {
   socket.on('join', slug => {
-    socket.join('has:' + slug)
+    socket.join('slug:' + slug)
     let room = rooms[slug]
     if (room) {
       for (let name in room) {
-        let size = room[name]
-        socket.emit('has', { slug, name, size })
+        let fileMeta = room[name]
+        socket.emit('has', { slug, name, ...fileMeta })
       }
     }
   })
   socket.on('leave', slug => {
-    socket.leave('has:' + slug)
+    socket.leave('slug:' + slug)
   })
-  socket.on('has', ({ slug, name, size }) => {
+  socket.on('has', data => {
+    let { slug, name, ...fileMeta } = data
     let room = (rooms[slug] ||= {})
-    room[name] = size
-    io.to('has:' + slug).emit('has', { slug, name, size })
+    room[name] = fileMeta
+    io.to('slug:' + slug).emit('has', data)
   })
+  function forwardData(event: string) {
+    socket.on(event, async data => {
+      let sockets = await io.in('slug:' + data.slug).fetchSockets()
+      for (let peerSocket of sockets) {
+        if (peerSocket.id == socket.id) continue
+        peerSocket.emit(event, data)
+      }
+    })
+  }
+  forwardData('want')
+  forwardData('content')
 })
 
 app.use(express.static('public'))
